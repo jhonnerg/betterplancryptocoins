5288be98f5dd77a8c678f3fca7cab609dac2d5f6
Renzo Zue <renzozue@outlook.com>
2021-04-15 16:43:37 -0400

    optimizacion de carga de datos y busqueda por un input en las lista de coins

--- src/app/modules/list-coins/list-coins.component.ts
+++ src/app/modules/list-coins/list-coins.component.ts
@@ -1,30 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { list_coins_model } from './list-coins.model';
 import { ServiceService } from './service.service';
 
 @Component({
-  selector: 'app-list-coins',
-  templateUrl: './list-coins.component.html',
-  styleUrls: ['./list-coins.component.scss']
+	selector: 'app-list-coins',
+	templateUrl: './list-coins.component.html',
+	styleUrls: ['./list-coins.component.scss'],
 })
 export class ListCoinsComponent implements OnInit {
+	public is_loadind: boolean = true;
+	public is_loadind_data: boolean = false;
+	public search_data: string = '';
+	List_Coins: list_coins_model[] = [];
+	List_Coins_Temp: list_coins_model[] = [];
+	List_Coins_filter: list_coins_model[] = [];
+	List_Coins_scroll: list_coins_model[] = [];
+	event_bool: boolean = false;
+	index_initial = 0;
+	Size = 10;
 
-  public is_loadind: boolean = true;
-  List_Coins: list_coins_model[] = [];
-  constructor(private service: ServiceService, private router: Router) { }
+	@HostListener('scroll', ['$event'])
+	onScroll(event: any) {
+		if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
+			this.ScrollPushData();
+		}
+	}
 
-  ngOnInit(): void {
-    this.List_coins()
-  }
+	constructor(private service: ServiceService, private router: Router) {}
 
-  List_coins() {
-    this.service.List_coins().subscribe((data: list_coins_model[]) => {
-      this.is_loadind = false;
-      this.List_Coins = data;
-    })
-  }
-  RoutelinkDetailCoins(id: any) {
-    this.router.navigate(['detail_coins', id])
-  }
+	ngOnInit(): void {
+		this.List_coins();
+	}
+
+	List_coins() {
+		this.service.List_coins().subscribe((data: list_coins_model[]) => {
+			this.is_loadind = false;
+			this.List_Coins = data;
+			this.ScrollPushData();
+		});
+	}
+
+	ScrollPushData() {
+      console.log("pasa")
+	if (this.search_data == '') {
+      this.is_loadind_data = true;
+		setTimeout(() => {
+			for (let index = this.index_initial; index < this.Size; index++) {
+				const element = this.List_Coins[index];
+				this.List_Coins_scroll.push(element);
+			}
+			this.List_Coins_Temp = this.List_Coins_scroll;
+			this.index_initial += 10;
+			this.Size += 10;
+			this.is_loadind_data = false;
+		}, 1500);
+   }
+	}
+
+	RoutelinkDetailCoins(id: any) {
+		this.router.navigate(['detail_coins', id]);
+	}
+
+	searchData(searchValue: any) {
+		if (this.search_data != '') {
+         this.List_Coins_filter = this.List_Coins.filter((item: list_coins_model) => {
+			 
+            return item.name.toLowerCase().includes(this.search_data.toLowerCase());
+         });
+         this.List_Coins_Temp = this.List_Coins_filter; 
+      } else {
+         this.List_Coins_Temp = this.List_Coins_scroll; 
+      }
+	}
 }
--- src/app/modules/list-coins/list-coins.module.ts
+++ src/app/modules/list-coins/list-coins.module.ts
@@ -8,6 +8,9 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider'
 import {MatButtonModule} from '@angular/material/button'
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     ListCoinsComponent,
@@ -20,7 +23,10 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ]
 })
 export class ListCoinsModule { }
